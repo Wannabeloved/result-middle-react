6ebef1fda50b71f9596c6387d8aed39139d9cca0
Rachev Artemii <quzyboy@gmail.com>
2026-01-17 12:05:13 +0300

    feat: migrate ContactListPage to MobX

--- src/pages/ContactListPage.tsx
+++ src/pages/ContactListPage.tsx
@@ -2,49 +2,32 @@ import React, {memo, useMemo} from 'react';
 import {Col, Row, Spinner, Alert} from 'react-bootstrap';
 import {ContactCard} from 'src/components/ContactCard';
 import {FilterForm, FilterFormValues} from 'src/components/FilterForm';
-import { useAppSelector, useAppDispatch } from 'src/store/hooks';
-import { setNameFilter, setGroupFilter } from 'src/store/filterSlice';
-import { useGetContactsQuery, useGetGroupsQuery } from 'src/store/api';
+import { observer } from 'mobx-react-lite';
+import { useStore } from 'src/store/RootStore';
 
-export const ContactListPage = memo(() => {
-  const dispatch = useAppDispatch();
-  const { data: contacts = [], isLoading: isContactsLoading, isError: isContactsError } = useGetContactsQuery();
-  const { data: groups = [], isLoading: isGroupsLoading, isError: isGroupsError } = useGetGroupsQuery();
-  const filter = useAppSelector(state => state.filter);
-  const loading = isContactsLoading || isGroupsLoading;
-  const error = isContactsError || isGroupsError ? 'Data loading error' : undefined;
-
-  const filteredContacts = useMemo(() => {
-    const { name, groupId } = filter;
-    const nameLower = name.toLowerCase();
-    
-    const group = groups.find(g => g.id === groupId);
-    const contactIdsInGroup = group ? new Set(group.contactIds) : null;
-
-    return contacts.filter(contact => {
-      const nameMatch = !nameLower || contact.name.toLowerCase().includes(nameLower);
-      const groupMatch = !contactIdsInGroup || contactIdsInGroup.has(contact.id);
-      return nameMatch && groupMatch;
-    });
-  }, [contacts, groups, filter]);
+export const ContactListPage = observer(() => {
+  const store = useStore();
+  const { filteredContacts, groups, filters, isLoading, error } = store;
+  const loading = isLoading.contacts || isLoading.groups;
+  const dataError = error.contacts || error.groups ? 'Data loading error' : undefined;
 
   const onSubmit = (fv: Partial<FilterFormValues>) => {
-    dispatch(setNameFilter(fv.name || ''));
-    dispatch(setGroupFilter(fv.groupId || ''));
+    store.setNameFilter(fv.name || '');
+    store.setGroupFilter(fv.groupId || '');
   };
 
   if (loading) {
     return <Spinner animation="border" />;
   }
 
-  if (error) {
-    return <Alert variant="danger">{error}</Alert>;
+  if (dataError) {
+    return <Alert variant="danger">{dataError}</Alert>;
   }
 
   return (
     <Row xxl={1}>
       <Col className="mb-3">
-        <FilterForm groupContactsList={groups} initialValues={filter} onSubmit={onSubmit} />
+        <FilterForm groupContactsList={groups} initialValues={filters} onSubmit={onSubmit} />
       </Col>
       <Col>
         <Row xxl={4} className="g-4">
